306298f02f28981697e87a780154e47232fb3c7c
Marin Capranov <marin.capranov@synectics-solutions.com>
2022-08-09 09:13:12 +0100

    Security matter

--- src/scrapping_service/scrapping_service/settings.py
+++ src/scrapping_service/scrapping_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sw104#^4(o&x0&to00goxh8gxih=s7st43#0*ps@e7z^yq+y@w'
+SECRET_KEY = '<INSERT HERE>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
