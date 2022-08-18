fb17cc4090b581d1f3b476960ca0518b753fe4a1
Marin Capranov <marin.capranov@synectics-solutions.com>
2022-08-18 14:45:30 +0100

    New admin fields

---
+++ src/scrapping_service/scrapping/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Name of the location')
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Name of the location'
+        verbose_name_plural = 'Name of the locations'
+
+    def __str__(self):
+        return self.name
+
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         self.slug =
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Programming language')
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Programming language'
+        verbose_name_plural = 'Programming languages'
+
+    def __str__(self):
+        return self.name
+
+
+class Vacancies(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Job title')
+    company = models.CharField(max_length=250, verbose_name='Company')
+    description = models.TextField(verbose_name='Job description')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='City')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name='Programming Language')
+    timestamp = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Vacancy'
+        verbose_name_plural = 'Vacancies'
+
+    def __str__(self):
+        return self.title
