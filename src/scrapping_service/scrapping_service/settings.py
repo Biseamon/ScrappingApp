309bdd0140b8eaf19552c410c05fc5e3d9d611bf
Marin Capranov <marin.capranov@synectics-solutions.com>
2022-08-19 16:50:00 +0100

    new admin panel and account class

--- src/scrapping_service/scrapping_service/settings.py
+++ src/scrapping_service/scrapping_service/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scrapping.apps.ScrappingConfig',
-    'accounts.apps.AccountsConfig',
+    'scrapping',
+    'accounts',
 ]
 
 MIDDLEWARE = [
